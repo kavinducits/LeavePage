bed064a9d940ac3dbf064236573d84a979c499e4
Kavindu Jayakodi <kavindujayakodi@sjp.ac.lk>
2026-01-06 09:13:00 +0530

    add comment

--- app/Http/Controllers/StudyLeaveController.php
+++ app/Http/Controllers/StudyLeaveController.php
@@ -18,6 +18,7 @@ class StudyLeaveController extends Controller
 
     /**
      * Show the form for creating a basic information .
+     * Calling Route: StudyLeave.create
      */
     public function createStudyLeave()
     {
