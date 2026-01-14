1c6af8227950daeb2f70d93ae0f226ca744ae80c
kavinducits <kavindujayakodi@sjp.ac.lk>
2026-01-14 16:20:02 +0530

    update

--- app/Http/Controllers/HODAcademicEstablishmentController.php
+++ app/Http/Controllers/HODAcademicEstablishmentController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 class HODAcademicEstablishmentController extends Controller
 {
     //
-    public function study_lave()
+    public function study_leave()
     {
+        
+
         return view('hod.academic_establishment.study_leave_index');
     }
 }
