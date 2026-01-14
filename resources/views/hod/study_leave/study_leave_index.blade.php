63df263cdda935dec611cc4078b357782712c98a
kavinducits <kavindujayakodi@sjp.ac.lk>
2026-01-14 08:11:10 +0530

    new update

--- resources/views/hod/study_leave/study_leave_index.blade.php
+++ resources/views/hod/study_leave/study_leave_index.blade.php
@@ -22,8 +22,7 @@
                 @endif
             </div>
             @include('hod.study_leave.study_leave_table')
-            @include('hod.study_leave.study_leave_progress_reports_table')
-            @include('hod.study_leave.study_leave_extensions_table')
+          
         </div>
     </section>
 </div>
